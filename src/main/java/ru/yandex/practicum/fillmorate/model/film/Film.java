7e5bc6c78bb322e191b61cb258dfaa5d8329bb2a
Semyon Senin <ily-senin@yandex.ru>
2022-09-27 23:04:05 +0300

    fix bugs

--- src/main/java/ru/yandex/practicum/fillmorate/model/film/Film.java
+++ src/main/java/ru/yandex/practicum/fillmorate/model/film/Film.java
@@ -17,7 +17,6 @@ public class Film {
     private Long id;
     private String name;
     private String description;
-    @Column(name = "release")
     private LocalDate releaseDate;
     private Long duration;
     private Set<Genre> genres;
